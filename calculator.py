2f08dc1dfa2b2b64f8c95d16479540d5efc9f2d8
Tamiron <tdrozdikova@gmail.com>
2026-03-01 22:00:36 +0300

    refactor: change multiplication and division function

--- calculator.py
+++ calculator.py
@@ -11,8 +11,11 @@ class Calculator:
             return self.first_number - self.second_number
         return self.second_number - self.first_number
 
-    def multipart(self):
+    def multiplication(self):
         return self.first_number * self.second_number
 
-    def delenie(self):
-        return self.first_number / self.second_number
+    def division(self, first_to_second=True):
+        zero_string = "Ошибка: Делимое число не может быть нулем"
+        if first_to_second:
+            return self.first_number / self.second_number if self.second_number else zero_string
+        return self.second_number / self.first_number if self.first_number else zero_string
